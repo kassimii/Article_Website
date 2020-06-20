97284b999d4809a2513a30d7d987d94c2184b6e9
kassimii <patyboc@yahoo.com>
2020-06-20 15:04:10 +0300

    Updates

---
+++ precache-manifest.3048b67164e7c765c02abe61649d5153.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3f245732c9677d22ee835fc6962f63",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "976ad60303aa569a4714",
+    "url": "/my-profile/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b7ee91aef1de621c9abe",
+    "url": "/my-profile/static/css/main.aab00007.chunk.css"
+  },
+  {
+    "revision": "976ad60303aa569a4714",
+    "url": "/my-profile/static/js/2.ae6398b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-profile/static/js/2.ae6398b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ee91aef1de621c9abe",
+    "url": "/my-profile/static/js/main.9483c605.chunk.js"
+  },
+  {
+    "revision": "c1d853bd7e89e86f5c06",
+    "url": "/my-profile/static/js/runtime-main.58046206.js"
+  },
+  {
+    "revision": "8074a9f13a7ec815aa3b7866f88c795d",
+    "url": "/my-profile/static/media/profile.8074a9f1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Article_Website/precache-manifest.6657b4463264f41f37634cd4ba910eb8.js"
+  "/my-profile/precache-manifest.3048b67164e7c765c02abe61649d5153.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Article_Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
